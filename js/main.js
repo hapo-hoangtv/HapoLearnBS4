26dec8b237c0c0745a80b8714bb4c1dbd6c3e08a
VietHoang <hoang.tv@haposoft.com>
2020-08-06 09:57:55 +0700

    Add Footer reponsive

--- js/main.js
+++ js/main.js
@@ -3,5 +3,23 @@ $(document).ready(function(){
         $('.icon').toggleClass('fa-times');
         $('.icon').toggleClass('fa-bars');
     });
+    $('.feedback-detail').slick({
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      adaptiveHeight: false,
+      autoplay: false,
+      autoplaySpeed : 3000,
+      prevArrow: $('.next-left'),
+      nextArrow: $('.next-right'),
+  
+      responsive: [
+        {
+          breakpoint: 769,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+  });
 });
-
